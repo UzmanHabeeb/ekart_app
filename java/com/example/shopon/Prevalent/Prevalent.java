fd2029f0745b3cc32d99ad23c942d6529ca65ead
Uzman <69146758+UzmanHabeeb@users.noreply.github.com>
2022-05-14 00:08:28 +0530

    Add files via upload

---
+++ java/com/example/shopon/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.shopon.Prevalent;
+
+import com.example.shopon.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
